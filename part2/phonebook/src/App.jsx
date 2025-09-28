de6fc128c77eb9918d59409c9475905cb53f3279
Kogetsuki <rozenbastien@gmail.com>
2025-09-28 11:45:20 +0200

    2.17 phonebook done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,17 +3,20 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
 
 
   useEffect(() => {
     personService
       .getPersons()
+
       .then(response =>
         setPersons(response.data))
   }, [])
@@ -32,16 +35,19 @@ const App = () => {
     if (personToUpdate === undefined) {
       personService
         .createPerson(personObject)
+
         .then(response => {
           setPersons(persons.concat(response.data))
           setNewName('')
           setNewNumber('')
+          showNotification(`Added ${personObject.name}`, 'success')
         })
     }
 
     else if (window.confirm(`${personObject.name} is already added to phonebook, replace the old number with the new one ?`)) {
       personService
         .updatePerson(personToUpdate.id, personObject)
+
         .then(response => {
           setPersons(persons.map(person =>
             person.id === personToUpdate.id
@@ -50,10 +56,29 @@ const App = () => {
           ))
           setNewName('')
           setNewNumber('')
+          showNotification(`Added ${personObject.name}`, 'success')
         })
+
         .catch(error =>
-          alert(error.response.data.error)
-        )
+          showNotification(error.response.data.error, 'error'))
+    }
+  }
+
+
+  const deletePerson = (id) => {
+    const personToDelete = persons.find(person => person.id === id)
+
+    if (window.confirm(`Delete ${personToDelete.name}?`)) {
+      personService
+        .deletePerson(id)
+
+        .then(() =>
+          setPersons(persons.filter(person => person.id !== id)))
+
+        .catch(() => {
+          setPersons(persons.filter(person => person.id !== id))
+          showNotification(`${personToDelete.name} was already removed from server`, 'error')
+        })
     }
   }
 
@@ -76,24 +101,15 @@ const App = () => {
     : persons
 
 
-  const deletePerson = (id) => {
-    const personToDelete = persons.find(person => person.id === id)
-
-    if (window.confirm(`Delete ${personToDelete.name}?`)) {
-      personService
-        .deletePerson(id)
-        .then(() =>
-          setPersons(persons.filter(person => person.id !== id)))
-        .catch(error => {
-          alert(`'${personToDelete.name}' was already removed from server`)
-          setPersons(persons.filter(person => person.id !== id))
-        })
-    }
+  const showNotification = (text, type, timeToShow = 3000) => {
+    setNotification({ text, type })
+    setTimeout(() => setNotification(null), timeToShow)
   }
 
 
   return (
     <>
+      <Notification notification={notification} />
       <h2>Phonebook</h2>
       <Filter
         value={filter}
---
+++ part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,22 @@
+const Notification = ({ notification }) => {
+  if (notification === null)
+    return null
+
+  const notificationStyle = {
+    color: notification.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: '20px',
+    borderStyle: 'solid',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '10px'
+  }
+
+  return (
+    <div style={notificationStyle}>
+      {notification.text}
+    </div>
+  )
+}
+
+export default Notification
