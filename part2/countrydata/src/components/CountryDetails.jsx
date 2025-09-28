66d385b6df4518ef97b12c3e090c0365d6828dad
Kogetsuki <rozenbastien@gmail.com>
2025-09-28 14:22:55 +0200

    part 2 finished

--- part2/countrydata/src/components/CountryDetails.jsx
+++ part2/countrydata/src/components/CountryDetails.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react'
+import weatherService from '../services/weather'
+
 const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    if (country.capital) {
+      weatherService
+        .getWeather(country.capital[0])
+
+        .then(response =>
+          setWeather(response.data))
+
+        .catch(err => console.error(err))
+    }
+  }, [country])
+
   return (
     <>
       <h1>{country.name.common}</h1>
-      <div>Capital {country.capital}</div>
+      <div>Capital {country.capital[0]}</div>
       <div>Area {country.area}</div>
 
       <h2>Languages</h2>
@@ -18,6 +35,18 @@ const CountryDetails = ({ country }) => {
         src={country.flags.png}
         alt={`Flag of ${country.name.common}`}
       />
+
+      {weather && (
+        <>
+          <h2>Weather in {country.capital[0]}</h2>
+          <div>Temperature {weather.main.temp} Celsius</div>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <div>Wind {weather.wind.speed} m/s</div>
+        </>
+      )}
     </>
   )
 }
