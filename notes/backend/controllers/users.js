3260c4149f546bb656e9ee2953f502e9cafa85b7
Kogetsuki <rozenbastien@gmail.com>
2025-10-19 20:08:28 +0200

    COURS populate

--- notes/backend/controllers/users.js
+++ notes/backend/controllers/users.js
@@ -4,7 +4,11 @@ const User = require('../models/user')
 
 
 usersRouter.get('/', async (req, res) => {
-  const users = await User.find({})
+  const users =
+    await User
+      .find({})
+      .populate('notes', { content: 1, important: 1 })
+
   res.json(users)
 })
 
