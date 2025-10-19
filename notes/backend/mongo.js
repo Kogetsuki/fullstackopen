e06fc0790f621cbbe58f1fa3acd90412fc6fbc74
Kogetsuki <rozenbastien@gmail.com>
2025-10-19 19:56:36 +0200

    COURS creating new note

--- notes/backend/mongo.js
+++ notes/backend/mongo.js
@@ -9,9 +9,31 @@ mongoose.connect(url)
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ]
+})
+
+
 const Note = mongoose.model('Note', noteSchema)
+const User = mongoose.model('User', userSchema)
 
 const notes = [
   { content: 'HTML is easy', important: true },
@@ -23,6 +45,18 @@ Note.insertMany(notes)
     console.log('notes saved')
     mongoose.connection.close()
   })
+
+// Note.deleteMany({})
+//   .then(() => {
+//     console.log('notes deleted')
+//     mongoose.connection.close()
+//   })
+
+// User.deleteMany({})
+//   .then(() => {
+//     console.log('users deleted')
+//     mongoose.connection.close()
+//   })
 
 // Note.find({}).then((result) => {
 //   result.forEach((note) => {
