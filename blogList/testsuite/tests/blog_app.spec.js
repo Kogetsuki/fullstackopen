cad6e7ba5b8001078e89e1faf00ef7c8114a10d6
Kogetsuki <rozenbastien@gmail.com>
2025-10-25 21:18:57 +0200

    BlogList e2e --> 5.22 step6: delete only by creator

--- blogList/testsuite/tests/blog_app.spec.js
+++ blogList/testsuite/tests/blog_app.spec.js
@@ -41,9 +41,51 @@ describe('Blog app', () => {
       await loginWith(page, 'mluukkai', 'salainen')
     })
 
-    test.only('a new blog can be created', async ({ page }) => {
+    test('a new blog can be created', async ({ page }) => {
       await createBlog(page, 'Test blog', 'Playwright', 'http://playwrightblog.com')
       await expect(page.locator('.blog', { hasText: 'Test blog' })).toBeVisible()
+    })
+
+    describe('a blog exists', () => {
+      beforeEach(async ({ page }) => {
+        await createBlog(page, 'Test blog', 'Playwright', 'http://playwrightblog.com')
+      })
+
+      test('a blog can be liked', async ({ page }) => {
+        await page.getByRole('button', { name: 'View' }).click()
+        const likesDiv = page.locator('text=likes')
+        const initialLikesText = await likesDiv.textContent()
+        const initialLikes = parseInt(initialLikesText.match(/\d+/)[0])
+        await page.getByRole('button', { name: 'Like' }).click()
+
+        await expect(likesDiv).toContainText(`likes ${initialLikes + 1}`)
+      })
+
+      test('a blog can be deleted by who created it', async ({ page }) => {
+        const blogLocator = page.locator('.blog', { hasText: 'Test blog' })
+        await page.getByRole('button', { name: 'View' }).click()
+        page.once('dialog', async dialog => {
+          await dialog.accept() // confirm the deletion
+        })
+        await page.getByRole('button', { name: 'Remove' }).click()
+
+        await expect(blogLocator).not.toBeVisible()
+      })
+
+      test.only('only the user who created the blog sees its delete button', async ({ page, request }) => {
+        await page.getByRole('button', { name: 'Logout' }).click()
+        await request.post('/api/users', {
+          data: {
+            name: 'Other user',
+            username: 'otheruser',
+            password: 'password'
+          }
+        })
+        await loginWith(page, 'otheruser', 'password')
+        await page.getByRole('button', { name: 'View' }).click()
+
+        await expect(page.getByRole('button', { name: 'Remove' })).not.toBeVisible()
+      })
     })
   })
 })
