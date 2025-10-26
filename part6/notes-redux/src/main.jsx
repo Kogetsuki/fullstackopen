43f8f05724a5b27a1758c0208e270e911c3fc387
Kogetsuki <rozenbastien@gmail.com>
2025-10-26 02:40:38 +0200

    COURS: notes-redux separate components with provider, useDispatch, useSelector

--- part6/notes-redux/src/main.jsx
+++ part6/notes-redux/src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App'
 import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
-
 
 store.dispatch({
   type: 'NEW_NOTE',
@@ -24,24 +26,8 @@ store.dispatch({
 })
 
 
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map(note => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () =>
-  root.render(<App />)
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
