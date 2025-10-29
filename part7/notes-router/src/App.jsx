38e18f39090260ddde934d49200c7f30f9ce536f
Kogetsuki <rozenbastien@gmail.com>
2025-10-29 16:00:23 +0100

    reset notes-router

--- part7/notes-router/src/App.jsx
+++ part7/notes-router/src/App.jsx
@@ -5,76 +5,15 @@ import {
   Route,
   Link,
   Navigate,
-  useNavigate,
   useMatch
 } from "react-router-dom"
 
+import Home from './components/Home'
+import Users from './components/Users'
+import Login from './components/Login'
+import Note from './components/Note'
+import Notes from './components/Notes'
 
-const Home = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-  </div>
-)
-
-const Note = ({ note }) => {
-  return (
-    <div>
-      <h2>{note.content}</h2>
-      <div>{note.user}</div>
-      <div><strong>{note.important ? 'important' : ''}</strong></div>
-    </div>
-  )
-}
-
-const Notes = ({ notes }) => (
-  <div>
-    <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
-  </div>
-)
-
-const Users = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <ul>
-      <li>Matti Luukkainen</li>
-      <li>Juha Tauriainen</li>
-      <li>Arto Hellas</li>
-    </ul>
-  </div>
-)
-
-const Login = (props) => {
-  const navigate = useNavigate()
-
-  const onSubmit = (event) => {
-    event.preventDefault()
-    props.onLogin('mluukkai')
-    navigate('/')
-  }
-
-  return (
-    <div>
-      <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
-  )
-}
 
 const App = () => {
   const [notes, setNotes] = useState([
