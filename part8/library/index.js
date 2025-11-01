1eb9706ad02ee7379e7fc5635d68eff967e8671d
Kogetsuki <rozenbastien@gmail.com>
2025-11-01 20:26:02 +0100

    Adding a book

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v4: uuid } = require('uuid')
 
 
 let authors = [
@@ -117,6 +118,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]
     allAuthors: [Author!]
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 
@@ -144,6 +154,21 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter(b => b.author === root.name).length
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(a => a.name === args.author))
+        authors = [
+          ...authors,
+          { name: args.author, id: uuid() }
+        ]
+
+      const newBook = { ...args, id: uuid() }
+      books = [...books, newBook]
+
+      return newBook
+    }
   }
 }
 
