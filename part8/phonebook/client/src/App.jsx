3e13a193b5bcc291ef77a33d537b0e0156600a72
Kogetsuki <rozenbastien@gmail.com>
2025-11-03 11:28:09 +0100

    Apollo client setup and simple query

---
+++ part8/phonebook/client/src/App.jsx
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+
+import Persons from './components/Persons'
+
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)
+    return <div>loading...</div>
+
+  return (
+    <div>
+      <Persons persons={result.data.allPersons} />
+    </div>
+  )
+}
+
+
+export default App
---
+++ part8/phonebook/client/src/components/Persons.jsx
@@ -0,0 +1,16 @@
+const Persons = ({ persons }) => {
+  return (
+    <>
+      <h2>Persons</h2>
+
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>
+      )}
+    </>
+  )
+}
+
+
+export default Persons
