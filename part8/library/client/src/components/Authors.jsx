46ac728913aa6d6856e56866cbbb10a0b6d3570f
Kogetsuki <rozenbastien@gmail.com>
2025-11-04 16:16:17 +0100

    author birthyear advanced with html select dropdown

--- part8/library/client/src/components/Authors.jsx
+++ part8/library/client/src/components/Authors.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client/react'
 import { ALL_AUTHORS } from '../queries'
+import BirthyearForm from './BirthyearForm'
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
@@ -34,6 +35,8 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+
+      <BirthyearForm />
     </>
   )
 }
---
+++ part8/library/client/src/components/BirthyearForm.jsx
@@ -0,0 +1,69 @@
+import { useQuery, useMutation } from '@apollo/client/react'
+import { useField } from '../hooks'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+
+const BirthyearForm = () => {
+  const name = useField('text')
+  const born = useField('number')
+
+  const result = useQuery(ALL_AUTHORS)
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  if (result.loading)
+    return <div>loading...</div>
+
+  const authors = result.data.allAuthors
+
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    if (!name.value)
+      return alert('Select an author')
+
+    await editAuthor({
+      variables: {
+        name: name.value,
+        setBornTo: Number(born.value)
+      }
+    })
+
+    name.reset()
+    born.reset()
+  }
+
+
+  return (
+    <>
+      <h2>Set birthyear</h2>
+
+      <form onSubmit={submit}>
+        <div>
+          Name
+          <select {...name.input}>
+            <option value=''>-- Choose author --</option>
+            {authors.map(a => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          Born
+          <input {...born.input} />
+        </div>
+
+        <button type='submit'>Update author</button>
+      </form>
+    </>
+  )
+}
+
+
+export default BirthyearForm
