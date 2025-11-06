39d1f32d575052a60fb3df37c272292a1422f45c
Kogetsuki <rozenbastien@gmail.com>
2025-11-06 11:44:38 +0100

    all queries and mutations functional

--- part8/library/server/index.js
+++ part8/library/server/index.js
@@ -23,86 +23,6 @@ mongoose.connect(MONGODB_URI)
 
 
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  {
-    name: 'Joshua Kerievsky',
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  {
-    name: 'Sandi Metz',
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-
 const typeDefs = `
   type Book {
     title: String!
@@ -171,10 +91,10 @@ const resolvers = {
       Author.find({})
   },
 
-  // Author: {
-  //   bookCount: (root) =>
-  //     books.filter(b => b.author === root.name).length
-  // },
+  Author: {
+    bookCount: async (root) =>
+      await Book.countDocuments({ author: root._id })
+  },
 
   Mutation: {
     addBook: async (root, args) => {
@@ -201,23 +121,15 @@ const resolvers = {
     },
 
 
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name)
+    editAuthor: async (root, args) => {
+      const author = await Author.findOne({ name: args.name })
       if (!author)
         return null
 
-      const updatedAuthor = {
-        ...author,
-        born: args.setBornTo
-      }
+      author.born = args.setBornTo
+      await author.save()
 
-      authors = authors.map(a =>
-        a.name === updatedAuthor.name
-          ? updatedAuthor
-          : a
-      )
-
-      return updatedAuthor
+      return author
     }
   }
 }
