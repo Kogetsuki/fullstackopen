90de981bfb56698ed7b211d76550d04adaa81558
Kogetsuki <rozenbastien@gmail.com>
2025-11-14 17:41:28 +0100

    setup ts

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,22 +1,6 @@
 import { useState } from 'react'
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) =>
-    setValue(event.target.value)
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
 const App = () => {
-  const name = useField('text')
-  const born = useField('date')
-  const height = useField('number')
 
   return (
     <>
