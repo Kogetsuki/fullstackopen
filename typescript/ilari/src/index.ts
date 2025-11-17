a855a8d110450b2b90486c320604bfc7d9b386df
Kogetsuki <rozenbastien@gmail.com>
2025-11-17 14:54:23 +0100

    COURS: Node and JSON module + Utility entries

--- typescript/ilari/src/index.ts
+++ typescript/ilari/src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
+import diaryRouter from './routes/diaries';
 
 const app = express();
-app.use(express.json());
+const PORT = 3000;
 
-const PORT = 3003;
+app.use(express.json());
 
 
 app.get('/ping', (_req, res) => {
   console.log('Someone pinged here');
   res.send('pong');
 });
+
+
+app.use('/api/diaries', diaryRouter);
 
 
 app.listen(PORT, () =>
---
+++ typescript/ilari/src/services/diaryService.ts
@@ -0,0 +1,28 @@
+import diaryData from '../../data/entries';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
+
+const getEntries = (): DiaryEntry[] =>
+  diaries;
+
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] =>
+  diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility
+  }));
+
+
+const addDiary = () =>
+  null;
+
+
+export default {
+  getEntries,
+  getNonSensitiveEntries,
+  addDiary
+};
