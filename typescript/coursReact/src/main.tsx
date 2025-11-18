1f7352f40672b011b4c3472965006ceff06355e1
Kogetsuki <rozenbastien@gmail.com>
2025-11-18 17:04:46 +0100

    COURS: More type narrowing

--- typescript/coursReact/src/main.tsx
+++ typescript/coursReact/src/main.tsx
@@ -1,18 +1,8 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-// import App from './App.tsx';
-
-
-interface WelcomeProps {
-  name: string;
-}
-
-
-export const Welcome = (props: WelcomeProps) =>
-  <h1>Hello, {props.name}</h1>;
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Welcome name='Sarah' />
+  <App />
 );
