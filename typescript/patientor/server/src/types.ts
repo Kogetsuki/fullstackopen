f3b1fc56d7401f67d44332780007d75451ae1a8f
Kogetsuki <rozenbastien@gmail.com>
2025-11-21 11:44:57 +0100

    EX: 9.23 all types for Entry

--- typescript/patientor/server/src/types.ts
+++ typescript/patientor/server/src/types.ts
@@ -1,23 +1,14 @@
 import { z } from 'zod';
 import { NewPatientSchema } from './utils';
 
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
-}
-
+// -----------------------------------------------------------------------
+// Patient
+// -----------------------------------------------------------------------
 export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other'
 }
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
-
-}
-
 
 export interface Patient {
   id: string;
@@ -31,5 +22,64 @@ export interface Patient {
 
 
 export type NewPatient = z.infer<typeof NewPatientSchema>;
-
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+
+// -----------------------------------------------------------------------
+// Entry
+// -----------------------------------------------------------------------
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
--- typescript/patientor/server/src/utils.ts
+++ typescript/patientor/server/src/utils.ts
@@ -4,13 +4,18 @@ import { Gender, NewPatient } from "./types";
 // -----------------------------------------------------------------------
 // Type substitutions
 // -----------------------------------------------------------------------
+export const EntrySchema = z.object({
+  type: z.enum(['Hospital', 'HealthCheck', 'OccupationalHealthcare'])
+});
+
+
 export const NewPatientSchema = z.object({
   name: z.string(),
   gender: z.nativeEnum(Gender),
   occupation: z.string(),
   dateOfBirth: z.string().date(),
   ssn: z.string(),
-  entries: z.array(z.any()).optional().default([])
+  entries: z.array(EntrySchema).optional().default([])
 });
 
 
