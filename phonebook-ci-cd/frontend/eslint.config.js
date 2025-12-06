45e6d488c920ed431df27c3329d2e01612459f1c
Kogetsuki <rozenbastien@gmail.com>
2025-12-06 19:57:05 +0100

    prepare for deploy pipeline

--- phonebook-ci-cd/frontend/eslint.config.js
+++ phonebook-ci-cd/frontend/eslint.config.js
@@ -1,7 +1,42 @@
+// eslint.config.js
+import js from "@eslint/js"
 import globals from "globals"
+import reactPlugin from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
 import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,jsx}"],
+    ignores: ["dist/"],
+
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // optional & nice to have
+      "react/react-in-jsx-scope": "off", // not needed with React 17+
+      "react/prop-types": "off",
+    },
+  },
 ])
