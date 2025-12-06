c77eb16b862a46c6b63a0275e1430e6fb237d179
Kogetsuki <rozenbastien@gmail.com>
2025-12-06 20:27:49 +0100

    prepare for deploy pipeline v8

--- phonebook-ci-cd/backend/index.js
+++ phonebook-ci-cd/backend/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
-import Person from './models/person'
+import Person from './models/person.js'
 
 dotenv.config()
 const app = express()
